3e384d9aae4beae552f77da97fe8428c76640957
Ben Reilly <benreilly19@gmail.com>
2025-06-05 17:17:51 -0400

    remove dist

--- create-echo-app/dist/create-app.d.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateAppConfig {
-    projectName: string;
-    template: string;
-    useNpm?: boolean;
-    useYarn?: boolean;
-    usePnpm?: boolean;
-}
-export declare const createApp: (config: CreateAppConfig) => Promise<void>;
-//# sourceMappingURL=create-app.d.ts.map
