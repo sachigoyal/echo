2c1c7ac2b1fa6ae0c5b80c5ff10856dc68340ced
Jason <jason@hedmans.org>
2025-08-01 12:27:52 -0400

    add authjs provider

---
+++ echo-authjs-provider/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: ['./src/__tests__/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+    },
+  },
+});
