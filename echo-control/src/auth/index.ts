4c928ff18d2213b738e7cefcb275fe126b5b62ca
Jason <jason@hedmans.org>
2025-08-05 15:08:21 -0400

    add prisma schema

--- echo-control/src/auth/index.ts
+++ echo-control/src/auth/index.ts
@@ -1,7 +1,11 @@
 import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 
 import { providers } from './providers';
 
+import { db } from '@/lib/db';
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers,
+  adapter: PrismaAdapter(db),
 });
