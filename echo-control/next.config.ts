ae388e28eb61ec812e5014dc8563d893476f55c4
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-08-10 02:28:01 -0400

    smol fix

--- echo-control/next.config.ts
+++ echo-control/next.config.ts
@@ -5,7 +5,7 @@ import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 const CORS_HEADERS = [
   { key: 'Access-Control-Allow-Origin', value: '*' },
   { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-  { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+  { key: 'Access-Control-Allow-Headers', value: '*' },
 ];
 
 const nextConfig: NextConfig = {
