0c26a76fe83c75547b9d583d32e44269c8772c30
Jason <jason@hedmans.org>
2025-08-19 10:59:02 -0400

    add hydration boundary to navbar balance

--- echo-control/src/app/_components/navbar/balance.tsx
+++ echo-control/src/app/_components/navbar/balance.tsx
@@ -7,19 +7,21 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { Suspense } from 'react';
 
 import { Balance } from './balance-display';
-import { api } from '@/trpc/server';
+import { api, HydrateClient } from '@/trpc/server';
 
 export const BalanceButton = async () => {
   void api.user.balance.get.prefetch();
 
   return (
-    <Link href="/credits">
-      <Button variant="outline">
-        <Logo className="h-4 w-4 text-muted-foreground group-hover:text-primary transition-colors" />
-        <Suspense fallback={<Skeleton className="h-5 w-10" />}>
-          <Balance />
-        </Suspense>
-      </Button>
-    </Link>
+    <HydrateClient>
+      <Link href="/credits">
+        <Button variant="outline">
+          <Logo className="h-4 w-4 text-muted-foreground group-hover:text-primary transition-colors" />
+          <Suspense fallback={<Skeleton className="h-5 w-10" />}>
+            <Balance />
+          </Suspense>
+        </Button>
+      </Link>
+    </HydrateClient>
   );
 };
