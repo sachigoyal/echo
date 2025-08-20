16d23e24d1be36e022addb8d2ecdc5059cb39d7f
Jason <jason@hedmans.org>
2025-08-20 15:31:57 -0400

    stream credits page

--- echo-control/src/app/(app)/@authenticated/_components/layout/page-utils.tsx
+++ echo-control/src/app/(app)/@authenticated/_components/layout/page-utils.tsx
@@ -4,11 +4,13 @@ import { Separator } from '@/components/ui/separator';
 
 interface HeadingProps {
   title: string;
+  icon?: ReactNode;
   description?: string;
   actions?: ReactNode;
 }
 
 export const Heading: React.FC<HeadingProps> = ({
+  icon,
   title,
   description,
   actions,
@@ -16,11 +18,14 @@ export const Heading: React.FC<HeadingProps> = ({
   return (
     <>
       <div className="flex items-center justify-between max-w-4xl w-full px-2 pb-4 md:pb-6 mx-auto">
-        <div className="flex flex-col gap-2 w-full text-left">
-          <h1 className="text-4xl font-bold">{title}</h1>
-          {description && (
-            <p className="text-muted-foreground/80 text-sm">{description}</p>
-          )}
+        <div className="flex items-center gap-2">
+          {icon}
+          <div className="flex flex-col gap-2 w-full text-left">
+            <h1 className="text-4xl font-bold">{title}</h1>
+            {description && (
+              <p className="text-muted-foreground/80 text-sm">{description}</p>
+            )}
+          </div>
         </div>
         {actions}
       </div>
