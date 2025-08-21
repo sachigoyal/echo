29d12834a15d153b25cc4f0b88d85d9ba604a3e1
Ryan Sproule <rsproule23@gmail.com>
2025-08-21 10:56:18 -0400

    rm dep update

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: [
-      '@merit-systems/echo-react-sdk',
-    ],
-  },
 });
