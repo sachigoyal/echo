0b9e32b41ff699c1d254091762f30d4ebe25471f
Ben Reilly <benreilly19@gmail.com>
2025-08-27 10:17:54 -0400

    wip fix gemini

--- examples/next-sdk-example/src/echo/index.ts
+++ examples/next-sdk-example/src/echo/index.ts
@@ -4,7 +4,7 @@ const baseEchoUrl = process.env.NEXT_PUBLIC_ECHO_URL || 'https://echo.merit.syst
 const baseRouterUrl = process.env.NEXT_PUBLIC_ROUTER_URL || 'https://echo.router.merit.systems';
 const appId = process.env.NEXT_PUBLIC_ECHO_APP_ID || '60601628-cdb7-481e-8f7e-921981220348';
 
-export const { handlers, isSignedIn, openai, anthropic, getUser } = Echo({
+export const { handlers, isSignedIn, openai, anthropic, google, getUser } = Echo({
   appId,
   baseEchoUrl,
   baseRouterUrl,
