221319da1e82e340034ad0da51b6de83747f2814
Ben Reilly <benreilly19@gmail.com>
2025-08-27 09:30:14 -0400

    env var config

--- examples/next-sdk-example/src/echo/index.ts
+++ examples/next-sdk-example/src/echo/index.ts
@@ -1,7 +1,11 @@
 import Echo from '@merit-systems/echo-next-sdk';
 
+const baseEchoUrl = process.env.NEXT_PUBLIC_ECHO_URL || 'https://echo.merit.systems';
+const baseRouterUrl = process.env.NEXT_PUBLIC_ROUTER_URL || 'https://echo.router.merit.systems';
+const appId = process.env.NEXT_PUBLIC_ECHO_APP_ID || '60601628-cdb7-481e-8f7e-921981220348';
+
 export const { handlers, isSignedIn, openai, anthropic, getUser } = Echo({
-  appId: '60601628-cdb7-481e-8f7e-921981220348',
-  // baseEchoUrl: 'http://localhost:3001',
-  // baseRouterUrl: 'http://localhost:3070',
+  appId,
+  baseEchoUrl,
+  baseRouterUrl,
 });
