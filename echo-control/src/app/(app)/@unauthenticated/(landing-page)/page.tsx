33163d904834cebf7bc7247ce91b4498befbd2e6
Jason <jason@hedmans.org>
2025-08-28 20:15:51 -0400

    dont render landing page when authed

--- echo-control/src/app/(app)/@unauthenticated/(landing-page)/page.tsx
+++ echo-control/src/app/(app)/@unauthenticated/(landing-page)/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from '@/auth';
 import { Hero, Features, IntegrationSection, End } from './_components';
 
 export default async function LandingPage() {
+  if (await auth()) {
+    return null;
+  }
+
   return (
     <div className="max-w-4xl mx-auto">
       <End />
