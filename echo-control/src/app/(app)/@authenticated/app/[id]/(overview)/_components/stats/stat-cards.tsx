2bc798863ae33021de75b586e53743e7491758e4
Jason <jason@hedmans.org>
2025-09-03 11:00:08 -0400

    update stat cards

--- echo-control/src/app/(app)/@authenticated/app/[id]/(overview)/_components/stats/stat-cards.tsx
+++ echo-control/src/app/(app)/@authenticated/app/[id]/(overview)/_components/stats/stat-cards.tsx
@@ -1,71 +1,69 @@
 import { Card } from '@/components/ui/card';
 import { formatCurrency } from '@/lib/utils';
 import { api } from '@/trpc/server';
+import { BanknoteArrowUp, Brain, Hash, LucideIcon, Users } from 'lucide-react';
 import { Suspense } from 'react';
 
 interface Props {
   appId: string;
 }
 
-const stats = ['Profit', 'Requests', 'Users', 'Tokens'];
+interface Stat {
+  title: string;
+  Icon: LucideIcon;
+}
+
+const stats: Stat[] = [
+  { title: 'Profit', Icon: BanknoteArrowUp },
+  { title: 'Tokens', Icon: Brain },
+  { title: 'Users', Icon: Users },
+  { title: 'Requests', Icon: Hash },
+];
 
 export const StatsCards = async ({ appId }: Props) => {
   const overallStatsPromise = api.apps.app.stats.overall({ appId });
 
-  return (
-    <>
-      <StatCard
-        title={stats[0]}
-        value={overallStatsPromise.then(({ totalProfit }) =>
-          formatCurrency(totalProfit)
-        )}
-      />
-      <StatCard
-        title={stats[1]}
-        value={overallStatsPromise.then(({ transactionCount }) =>
-          transactionCount.toLocaleString(undefined, {
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 2,
-            notation: 'compact',
-          })
-        )}
-      />
-      <StatCard
-        title={stats[2]}
-        value={overallStatsPromise.then(({ numUsers }) =>
-          numUsers.toLocaleString(undefined, {
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 2,
-            notation: 'compact',
-          })
-        )}
-      />
-      <StatCard
-        title={stats[3]}
-        value={overallStatsPromise.then(({ totalTokens }) =>
-          totalTokens.toLocaleString(undefined, {
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 2,
-            notation: 'compact',
-          })
-        )}
-      />
-    </>
-  );
+  const values = [
+    overallStatsPromise.then(({ totalProfit }) => formatCurrency(totalProfit)),
+    overallStatsPromise.then(({ totalTokens }) =>
+      totalTokens.toLocaleString(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+        notation: 'compact',
+      })
+    ),
+    overallStatsPromise.then(({ numUsers }) =>
+      numUsers.toLocaleString(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+        notation: 'compact',
+      })
+    ),
+    overallStatsPromise.then(({ transactionCount }) =>
+      transactionCount.toLocaleString(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+        notation: 'compact',
+      })
+    ),
+  ];
+
+  return stats.map((stat, index) => (
+    <StatCard key={stat.title} {...stat} value={values[index]} />
+  ));
 };
 
 export const LoadingStatsCards = () => {
-  return stats.map(stat => <LoadingStatCard title={stat} key={stat} />);
+  return stats.map(stat => <LoadingStatCard {...stat} key={stat.title} />);
 };
 
-interface StatsCardProps {
-  title: string;
+interface StatsCardProps extends Stat {
   value: Promise<string>;
 }
 
-const StatCard = ({ title, value }: StatsCardProps) => {
+const StatCard = ({ value, ...stat }: StatsCardProps) => {
   return (
-    <BaseStatCard title={title}>
+    <BaseStatCard {...stat}>
       <Suspense fallback={<LoadingStatContent />}>
         <StatContent value={value} />
       </Suspense>
@@ -74,31 +72,34 @@ const StatCard = ({ title, value }: StatsCardProps) => {
 };
 
 const StatContent = async ({ value }: { value: Promise<string> }) => {
-  return <div className="text-2xl font-bold">{await value}</div>;
+  return <div className="text-4xl font-bold">{await value}</div>;
 };
 
-const LoadingStatCard = ({ title }: { title: string }) => {
+const LoadingStatCard = (stat: Stat) => {
   return (
-    <BaseStatCard title={title}>
+    <BaseStatCard {...stat}>
       <LoadingStatContent />
     </BaseStatCard>
   );
 };
 
 const LoadingStatContent = () => {
-  return <div className="text-2xl font-bold">Loading...</div>;
+  return <div className="text-xl font-bold">Loading...</div>;
 };
 
 const BaseStatCard = ({
   title,
+  Icon,
   children,
-}: {
-  title: string;
+}: Stat & {
   children: React.ReactNode;
 }) => {
   return (
-    <Card className="p-4">
-      <h4>{title}</h4>
+    <Card className="p-4 gap-2 flex flex-col">
+      <div className="flex items-center gap-1.5 text-muted-foreground">
+        <Icon className="size-4 shrink-0" />
+        <h4 className="text-sm font-medium">{title}</h4>
+      </div>
       {children}
     </Card>
   );
