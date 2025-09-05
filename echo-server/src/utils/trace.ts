6cfc4eba2c4d54a2a94db907dafacd44efc2a733
Ben Reilly <benreilly19@gmail.com>
2025-09-05 10:49:32 -0400

    yeet test cases

--- echo-server/src/utils/trace.ts
+++ echo-server/src/utils/trace.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
  * Get the current request ID from OpenTelemetry trace context
  * @returns The trace ID as request ID for correlation
  * @returns A random UUID if no trace context is available
- * 
+ *
  * This function should never throw an error, and should always return a valid request ID.
  * If OpenTelemetry is initialized, we will use the trace ID.
  * If OpenTelemetry is not initialized, we will use a random UUID.
