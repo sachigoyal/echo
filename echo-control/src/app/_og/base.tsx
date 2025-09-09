30119056943f5206b5e5a4f1b1f485a82a1fbce8
Jason <jason@hedmans.org>
2025-09-08 23:07:12 -0400

    add base og image

--- echo-control/src/app/_og/base.tsx
+++ echo-control/src/app/_og/base.tsx
@@ -2,6 +2,14 @@ import { ImageResponse } from 'next/og';
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
+export const backgroundColor = '#fcfcfc';
+export const borderColor = '#d4d4d4';
+export const dashWidth = '1px';
+export const strokeDasharray = '1 4';
+export const xPadding = 16;
+const yPadding = 16;
+const circleSize = 16;
+
 export const baseOgImage = async (component: React.ReactNode) => {
   const [
     helveticaNowDisplayBlack,
@@ -30,12 +38,101 @@ export const baseOgImage = async (component: React.ReactNode) => {
   return new ImageResponse(
     (
       <div
-        className="h-full w-full flex flex-col justify-center items-center"
+        tw="flex flex-col relative"
         style={{
-          background: 'white',
+          background: backgroundColor,
+          height: '100%',
+          width: '100%',
         }}
       >
-        {component}
+        {/* Top border */}
+        <div
+          tw={`h-${yPadding} w-full px-${xPadding} flex items-center`}
+          style={{
+            borderBottom: `${dashWidth} dashed ${borderColor}`,
+            strokeDasharray,
+          }}
+        >
+          <div
+            tw="flex-1 h-full"
+            style={{
+              borderLeft: `${dashWidth} dashed ${borderColor}`,
+              borderRight: `${dashWidth} dashed ${borderColor}`,
+            }}
+          />
+        </div>
+        <div
+          tw="top-0 left-0 right-0 z-10 h-4 absolute"
+          style={{
+            zIndex: 10,
+            background: `linear-gradient(to bottom, ${backgroundColor} 0%, rgba(252, 252, 252, 0) 100%)`,
+          }}
+        />
+        <div
+          tw={`absolute top-${yPadding - circleSize / 2} left-${xPadding - circleSize / 2} z-10 rounded-full h-${circleSize} w-${circleSize}`}
+          style={{
+            border: `${dashWidth} dashed ${borderColor}`,
+          }}
+        />
+        <div
+          tw={`absolute bottom-${yPadding - circleSize / 2} right-${xPadding - circleSize / 2} z-10 rounded-full h-${circleSize} w-${circleSize}`}
+          style={{
+            border: `${dashWidth} dashed ${borderColor}`,
+          }}
+        />
+
+        {/* Content */}
+        <div tw="flex-1 w-full flex">
+          <div tw={`w-${xPadding} h-full`} />
+          <div
+            tw="flex-1 flex flex-col"
+            style={{
+              borderLeft: `${dashWidth} dashed ${borderColor}`,
+              borderRight: `${dashWidth} dashed ${borderColor}`,
+            }}
+          >
+            <div
+              tw="flex-1 w-full flex flex-col"
+              style={{
+                background: backgroundColor,
+                zIndex: 11,
+              }}
+            >
+              {component}
+            </div>
+          </div>
+          <div tw={`w-${xPadding} h-full`} />
+        </div>
+
+        {/* Bottom border */}
+        <div
+          tw={`h-${yPadding} w-full px-${xPadding} flex items-center`}
+          style={{
+            borderTop: `${dashWidth} dashed ${borderColor}`,
+          }}
+        >
+          <div
+            tw="flex-1 h-full"
+            style={{
+              borderLeft: `${dashWidth} dashed ${borderColor}`,
+              borderRight: `${dashWidth} dashed ${borderColor}`,
+            }}
+          />
+        </div>
+
+        {/* Side gradients */}
+        <div
+          tw="bottom-0 top-0 left-0 w-4 flex items-center z-10 absolute"
+          style={{
+            background: `linear-gradient(to right, ${backgroundColor} 0%, rgba(252, 252, 252, 0) 100%)`,
+          }}
+        />
+        <div
+          tw="bottom-0 top-0 right-0 w-4 flex items-center z-10 absolute"
+          style={{
+            background: `linear-gradient(to left, ${backgroundColor} 0%, rgba(252, 252, 252, 0) 100%)`,
+          }}
+        />
       </div>
     ),
     {
--- echo-control/src/app/opengraph-image.tsx
+++ echo-control/src/app/opengraph-image.tsx
@@ -1,4 +1,10 @@
-import { baseOgImage } from './_og/base';
+import {
+  backgroundColor,
+  baseOgImage,
+  borderColor,
+  dashWidth,
+  xPadding,
+} from './_og/base';
 import { baseUrl } from './_og/base-url';
 
 // Image metadata
@@ -13,25 +19,83 @@ export const contentType = 'image/png';
 // Image generation
 export default async function Image() {
   return baseOgImage(
-    <div tw="flex flex-col w-full flex-1 justify-center items-center pb-8">
-      <img src={`${baseUrl}/logo/light.svg`} alt="Echo" tw="w-24 h-24" />
-      <h1
-        tw="m-0 mb-4 text-8xl"
+    <div tw="flex flex-col flex-1">
+      <div
+        tw="flex justify-center items-center"
         style={{
-          fontWeight: 800,
-          background:
-            'linear-gradient(to bottom, rgb(1, 154, 174) 0%, rgba(1, 154, 174, 0.7) 100%)',
-          WebkitBackgroundClip: 'text',
-          WebkitTextFillColor: 'transparent',
-          backgroundClip: 'text',
-          color: 'transparent',
+          borderBottom: `${dashWidth} dashed ${borderColor}`,
         }}
       >
-        Echo
-      </h1>
-      <h2 tw="m-0 text-4xl font-medium text-neutral-700">
-        Monetize AI Apps in Minutes
-      </h2>
+        <hr
+          tw="flex-1"
+          style={{
+            borderTop: `${dashWidth} dashed ${borderColor}`,
+            borderLeft: 'none',
+            borderRight: 'none',
+            borderBottom: 'none',
+            marginLeft: `-${xPadding * 4}`,
+          }}
+        />
+        <div
+          tw="flex flex-col p-8 rounded-full relative"
+          style={{
+            background: backgroundColor,
+            border: `${dashWidth} dashed ${borderColor}`,
+            marginTop: '-16px',
+            marginBottom: '-16px',
+          }}
+        >
+          <div
+            tw="rounded-full p-4 bg-white flex"
+            style={{
+              boxShadow: '0 0 16px 8px rgba(0,157,200,0.5)',
+              border: `1px solid ${borderColor}`,
+              marginTop: '-1px',
+            }}
+          >
+            <img
+              src={`${baseUrl}/logo/light.svg`}
+              alt="Echo"
+              height={128}
+              width={128}
+            />
+          </div>
+        </div>
+        <hr
+          tw="flex-1"
+          style={{
+            borderTop: `${dashWidth} dashed ${borderColor}`,
+            marginRight: `-${xPadding * 4}`,
+          }}
+        />
+      </div>
+      <div tw="flex flex-col justify-center items-center flex-1">
+        <h1
+          tw="text-8xl font-extrabold m-0 mb-2"
+          style={{
+            background:
+              'linear-gradient(to bottom, #009dc8 0%, rgba(0,157,200,0.6) 100%)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            backgroundClip: 'text',
+            color: 'transparent',
+          }}
+        >
+          Echo
+        </h1>
+        <h2
+          tw="m-0 text-4xl font-bold mb-12"
+          style={{
+            opacity: 0.8,
+          }}
+        >
+          Monetize AI Apps in Minutes
+        </h2>
+        <h3 tw="m-0 text-2xl">
+          <span style={{ fontWeight: 500 }}> Merit</span>
+          <span tw="font-light">Systems</span>
+        </h3>
+      </div>
     </div>
   );
 }
