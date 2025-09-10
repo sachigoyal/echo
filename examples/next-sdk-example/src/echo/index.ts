fa698ce485f6b527089b111f089e15515f3b9e45
sragss <sammage@gmail.com>
2025-09-10 16:34:22 -0400

    don't point at local

--- examples/next-sdk-example/src/echo/index.ts
+++ examples/next-sdk-example/src/echo/index.ts
@@ -1,14 +1,8 @@
 import Echo from '@merit-systems/echo-next-sdk';
 
-const baseEchoUrl = process.env.NEXT_PUBLIC_ECHO_URL || 'http://localhost:3000';
-const baseRouterUrl =
-  process.env.NEXT_PUBLIC_ROUTER_URL || 'http://localhost:3070';
 const appId =
-  process.env.NEXT_PUBLIC_ECHO_APP_ID || '4bc54846-9b96-4c78-b132-117903afc974';
+  process.env.NEXT_PUBLIC_ECHO_APP_ID || '74d9c979-e036-4e43-904f-32d214b361fc';
 
-export const { handlers, isSignedIn, openai, anthropic, google, getUser } =
-  Echo({
+export const { handlers, isSignedIn, openai, anthropic, google, getUser } = Echo({
     appId,
-    baseEchoUrl,
-    baseRouterUrl,
   });
