663014657b04d58dc9f0b353c72c2af2a63eba58
Ryan Sproule <rsproule23@gmail.com>
2025-09-15 18:54:37 -0400

    rm console log

--- packages/sdk/react/src/hooks/useChat.ts
+++ packages/sdk/react/src/hooks/useChat.ts
@@ -1,16 +1,12 @@
 import { useChat as useChatBase } from '@ai-sdk/react';
 import { type ChatTransport, type UIMessage } from 'ai';
-import {
-  ChatSendParams,
-  useEchoChatConfig,
-} from '../components/EchoChatProvider';
+import { useEchoChatConfig } from '../components/EchoChatProvider';
 
 function createInMemoryChatTransport(): ChatTransport<UIMessage> {
   const chatFn = useEchoChatConfig();
   return {
     sendMessages: async options => {
-      console.log('sendMessages', options);
-      return chatFn(options as ChatSendParams);
+      return chatFn(options);
     },
     async reconnectToStream() {
       return null;
