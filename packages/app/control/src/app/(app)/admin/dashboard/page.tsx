a6af82868cd079cdd7499123b97b456f72eff5ae
Ben Reilly <benreilly19@gmail.com>
2025-09-17 17:27:02 -0400

    remove unused

--- packages/app/control/src/app/(app)/admin/dashboard/page.tsx
+++ packages/app/control/src/app/(app)/admin/dashboard/page.tsx
@@ -1,21 +1,4 @@
 import { userOrRedirect } from '@/auth/user-or-redirect';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import {
-  TrendingUp,
-  TrendingDown,
-  Code,
-  CreditCard,
-  Users,
-} from 'lucide-react';
-import Link from 'next/link';
-import { TableLayout } from '@/app/(app)/admin/_components';
 import TotalTokensChart from '@/app/(app)/admin/_components/chart/TotalTokens';
 
 export default async function AdminDashboard(
