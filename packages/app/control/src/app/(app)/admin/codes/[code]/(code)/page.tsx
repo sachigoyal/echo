36607c7e9f41c60c877fb6aaec38e3973d28be23
Jason <jason@hedmans.org>
2025-09-17 15:35:12 -0400

    add claim link section to grant page

--- packages/app/control/src/app/(app)/admin/codes/[code]/(code)/page.tsx
+++ packages/app/control/src/app/(app)/admin/codes/[code]/(code)/page.tsx
@@ -4,8 +4,17 @@ import { checkCreditGrant } from '../_lib/checks';
 import { EditCreditGrantForm } from './_components/edit-form';
 import { DisableCreditGrantDialog } from './_components/disable-dialog';
 import { Button } from '@/components/ui/button';
-import { Share, Trash } from 'lucide-react';
+import { QrCode, Share, Trash } from 'lucide-react';
 import Link from 'next/link';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from '@/components/ui/card';
+import { CopyCode } from '@/components/ui/copy-code';
 
 export default async function AdminCodePage(
   props: PageProps<'/admin/codes/[code]'>
@@ -18,25 +27,43 @@ export default async function AdminCodePage(
     <div>
       <Heading
         title="Credit Grant"
-        description="Scan this QR code to claim free Echo credits"
+        description="Edit this credit grant"
         actions={
-          <div className="flex items-center gap-2">
-            <DisableCreditGrantDialog creditGrantId={creditGrant.id}>
-              <Button variant="destructiveOutline">
-                <Trash className="size-4" />
-                Disable
-              </Button>
-            </DisableCreditGrantDialog>
-            <Link href={`/admin/codes/${code}/share`} passHref>
-              <Button variant="turbo">
-                <Share className="size-4" />
-                Share
-              </Button>
-            </Link>
-          </div>
+          <DisableCreditGrantDialog creditGrantId={creditGrant.id}>
+            <Button variant="destructiveOutline">
+              <Trash className="size-4" />
+              Disable
+            </Button>
+          </DisableCreditGrantDialog>
         }
       />
       <Body>
+        <Card>
+          <CardHeader className="border-b">
+            <CardTitle>Claim Link</CardTitle>
+            <CardDescription>
+              Users can claim this credit grant by visiting this link.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="py-4">
+            <CopyCode
+              code={`${process.env.ECHO_CONTROL_APP_BASE_URL}/credits/claim/${code}`}
+              toastMessage="Copied to clipboard"
+            />
+          </CardContent>
+          <CardFooter className="border-t py-4">
+            <Link
+              href={`/admin/codes/${code}/share`}
+              passHref
+              className="w-full"
+            >
+              <Button variant="turbo" className="font-bold w-full" size="lg">
+                <QrCode className="size-4" />
+                See QR Code
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
         <EditCreditGrantForm id={creditGrant.id} creditGrant={creditGrant} />
       </Body>
     </div>
