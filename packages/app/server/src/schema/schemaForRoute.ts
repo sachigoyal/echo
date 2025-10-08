517058a6fa18b513b02fe1c81402ad41a9883971
Ben Reilly <benreilly19@gmail.com>
2025-10-08 12:39:23 -0400

    add image schema

--- packages/app/server/src/schema/schemaForRoute.ts
+++ packages/app/server/src/schema/schemaForRoute.ts
@@ -2,6 +2,7 @@ import { OpenAIVideoCreateParamsSchema, OpenAIVideoSchema } from "./video/openai
 import { GeminiFlashImageInputSchema, GeminiFlashImageOutputSchema } from "./image/gemini";
 import { z } from "zod";
 import { ChatCompletionInput, ChatCompletionOutput } from "./chat/completions";
+import { CreateImagesRequest, CreateImagesResponse } from "./image/openai";
 
 export function getSchemaForRoute(path: string): { input: unknown, output: unknown } | undefined {
     console.log('path', path);
@@ -9,6 +10,13 @@ export function getSchemaForRoute(path: string): { input: unknown, output: unkno
         const returnObject = {
             input: z.toJSONSchema(OpenAIVideoCreateParamsSchema),
             output: z.toJSONSchema(OpenAIVideoSchema),
+        }
+        return returnObject;
+    }
+    if (path.endsWith("/images")) {
+        const returnObject = {
+            input: z.toJSONSchema(CreateImagesRequest),
+            output: z.toJSONSchema(CreateImagesResponse),
         }
         return returnObject;
     }
